dc8e7e2f469030f3cab8201691bcdb7332a6755a
root <root@LAPTOP-FA299JUM.localdomain>
2024-04-27 05:34:29 +0800

    Styled page layout n a little functionality

--- static/sampling/sampling.js
+++ static/sampling/sampling.js
@@ -1,34 +1,22 @@
 function copyTextareaContent() {
-    // Select the textarea
     var textarea = document.getElementById("results");
-
-    // Select its content
     textarea.select();
-
-    // Copy the selected text
     document.execCommand("copy");
-
-    // Deselect the textarea
     textarea.setSelectionRange(0, 0);
-
-    // Alert the user that the text has been copied
-    alert("Text copied to clipboard!");
 }
 
-function adjustTextareaHeight(textarea, maxHeight) {
-    // Reset the height to auto to shrink it if needed
-    textarea.style.height = 'auto';
-    // Set the height to the minimum value between scroll height and max height
-    textarea.style.height = Math.min(textarea.scrollHeight, maxHeight) + 'px';
+function clearForm() {
+    // Get the form element
+    var form = document.getElementById("excel-form");
+
+    // Loop through all form elements
+    for (var i = 0; i < form.elements.length; i++) {
+        var element = form.elements[i];
+
+        // Check if the element is an input
+        if (element.tagName === "INPUT") {
+            // Clear the value of the element
+            element.value = "";
+        }
+    }
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-    var textarea = document.getElementById('results');
-    var maxHeight = 200;
-  
-    textarea.addEventListener('input', function() {
-        adjustTextareaHeight(this, maxHeight);
-    });
-
-    adjustTextareaHeight(textarea, maxHeight);
-});
